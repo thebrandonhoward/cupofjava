919e90d74a0ae04407ccfaaa06ab57e2a30b53be
thebrandonhoward <thebrandonhoward@outlook.com>
2024-10-15 19:43:45 -0500

    Adds leetcode 4

---
+++ src/main/java/com/thebrandonhoward/cupofjava/challenges/CanPlaceFlowers.java
@@ -0,0 +1,64 @@
+package com.thebrandonhoward.cupofjava.challenges;
+
+public class CanPlaceFlowers {
+
+    public static void main(String... args) {
+        int[] z = {0,0,1,0,1};
+        int[] x = {1,0,0,0,0,0,1};
+        int[] y = {1,0,0,0,1};
+
+        System.out.println(canPlaceFlowers(z, 1));
+        System.out.println(canPlaceFlowers(x, 2));
+        System.out.println(canPlaceFlowers(y, 1));
+        System.out.println(canPlaceFlowers(y, 2));
+    }
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+            if(n == 0)
+                return true;
+
+            int previousIndex = 0;
+            int nextIndex = 0;
+            int amountPlanted = 0;
+            int overflow = flowerbed.length;
+
+            for(int currentIndex=0; currentIndex<flowerbed.length; currentIndex++) {
+                nextIndex = currentIndex + 1;
+
+                if(currentIndex == previousIndex) {
+                    if(nextIndex < overflow) {
+                        if(flowerbed[currentIndex] == 0 && flowerbed[nextIndex] == 0) {
+                            flowerbed[currentIndex] = 1;
+                            amountPlanted++;
+                        }
+                    }
+                    else {
+                        if(flowerbed[currentIndex] == 0 && n == 1) {
+                            amountPlanted++;
+                            return amountPlanted >= n;
+                        }
+                    }
+                }
+                else {
+                    if(nextIndex < overflow) {
+                        if((flowerbed[previousIndex] == 0) && (flowerbed[currentIndex] == 0) && flowerbed[nextIndex] == 0) {
+                            flowerbed[currentIndex] = 1;
+                            amountPlanted++;
+                        }
+                    }
+                    else {
+                        if((flowerbed[previousIndex] == 0) && (flowerbed[currentIndex] == 0)) {
+                            flowerbed[currentIndex] = 1;
+                            amountPlanted++;
+
+                            return amountPlanted >= n;
+                        }
+                    }
+                }
+
+                previousIndex = currentIndex;
+            }
+
+            return amountPlanted >= n;
+        }
+
+}
