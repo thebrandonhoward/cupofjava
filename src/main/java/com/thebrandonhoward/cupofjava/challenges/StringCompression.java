4d989d1f8fa8d515b0a802de55e669263f5d203d
thebrandonhoward <thebrandonhoward@outlook.com>
2024-10-24 22:00:37 -0500

    Adds leetcode 9

---
+++ src/main/java/com/thebrandonhoward/cupofjava/challenges/StringCompression.java
@@ -0,0 +1,39 @@
+package com.thebrandonhoward.cupofjava.challenges;
+
+//9 | https://leetcode.com/problems/string-compression/submissions/1432994978/?envType=study-plan-v2&envId=leetcode-75
+public class StringCompression {
+    public static void main(String[] args) {
+        System.out.println(compress(new char[]{'a','a','b','b','c','c','c'}));
+        System.out.println(compress(new char[]{'a'}));
+    }
+
+    public static int compress(char[] chars) {
+        int charCount;
+        char previousChar;
+        int currentIndex = 0;
+        int charsIndex = 0;
+
+        while(currentIndex < chars.length) {
+            charCount = 0;
+            previousChar = chars[currentIndex];
+
+            while((currentIndex < chars.length) && (chars[currentIndex] == previousChar)) {
+                charCount++;
+                currentIndex++;
+            }
+
+            chars[charsIndex] = previousChar;
+
+            if(charCount > 1) {
+                for(char num : String.valueOf(charCount).toCharArray()) {
+                    charsIndex++;
+                    chars[charsIndex] = num;
+                }
+            }
+
+            charsIndex++;
+        }
+
+        return charsIndex;
+    }
+}
